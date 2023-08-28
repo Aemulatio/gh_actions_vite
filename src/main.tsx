17b830009f152f0a31237f8fcfa7b102d6695de5
Stipan <dumn2011@Gmail.com>
2023-08-28 10:12:31 +0300

    add: react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Test} from "./pages/Test/Test.tsx";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <BrowserRouter basename={"/gh_actions_vite/"}>
+            <Routes>
+                <Route path={"/"} element={<App/>}/>
+                <Route path={"/test"} element={<Test/>}/>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
 )
---
+++ src/pages/Test/Test.tsx
@@ -0,0 +1,8 @@
+import {Link} from "react-router-dom";
+
+
+export const Test = () => {
+    return <>
+        <Link to={"/"}>HOME</Link>
+    </>
+}
